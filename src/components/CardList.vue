1e023e7fe51c3e9a6a013d01326044e08c40b3a4
Kirill <pesterevkv2003@gmail.com>
2025-01-17 23:15:51 +0300

    fix some errors with TS

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
 import { useCardsStore } from "../store/cardStore";
-import type { ICard } from "../interfaces/ICard";
-import { watch } from "vue";
 
 const { cards } = storeToRefs(useCardsStore());
 </script>
